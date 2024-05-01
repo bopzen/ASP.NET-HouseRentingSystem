557ffd18f8dd44d8c0654493e106480d1b126ae5
bopzen <boppp@abv.bg>
2024-05-01 21:57:51 +0300

    Added all HouseController methods, view models and views

--- HouseRentingSystem/Controllers/HomeController.cs
+++ HouseRentingSystem/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using HouseRentingSystem.Models;
+using HouseRentingSystem.Models.Home;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,15 +7,10 @@ namespace HouseRentingSystem.Controllers
 {
     public class HomeController : Controller
     { 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return View(new IndexViewModel());
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
